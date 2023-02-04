2e5889123f726e4565188c069713188d8029a0cd
alfytka <lyzexx04@gmail.com>
2023-02-04 11:12:54 +0700

    footer

--- resources/views/layouts/admin/kerangka.blade.php
+++ resources/views/layouts/admin/kerangka.blade.php
@@ -67,15 +67,13 @@
 
   </main><!-- End #main -->
 
-  <!-- ======= Footer ======= 
+  {{-- ======= Footer =======  
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+      &copy; Copyright 2023 - <strong><span>@Alfitka</span></strong>
     </div>
-    <div class="credits">
-      Redesigned by <a href="https://instagram.com/alfytka_">Alfitka</a>
-    </div>
-  </footer>End Footer -->
+  </footer> 
+   End Footer --> --}}
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
