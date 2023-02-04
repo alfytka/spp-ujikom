92167a8ec921670a96945024aa2c57ae75c66945
alfytka <lyzexx04@gmail.com>
2023-02-04 11:06:17 +0700

    validasi input data siswa

---
+++ app/Http/Requests/SiswaRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SiswaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nisn' => ['required', 'string'],
+            'nis' => ['required', 'string'],
+            'name' => ['required', 'string'],
+            'kelas_id' => ['required'],
+            'spp_id' => ['required'],
+            'email' => ['required', 'string'],
+            'username' => ['required', 'string'],
+            'password' => ['required', 'string'],
+            'telepon' => ['string'],
+            'alamat' => ['string'],
+        ];
+    }
+}
