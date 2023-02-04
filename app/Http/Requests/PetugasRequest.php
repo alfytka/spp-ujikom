7ca51fe12c8a29f25fed4a5452cd1dfd9d894f40
alfytka <lyzexx04@gmail.com>
2023-02-04 11:05:16 +0700

    validasi data petugas

---
+++ app/Http/Requests/PetugasRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PetugasRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'string'],
+            'username' => ['required', 'string'],
+            'password' => ['required', 'string'],
+            'telepon' => ['required', 'numeric'],
+            'alamat' => ['required', 'string'],
+            'level' => ['required']
+        ];
+    }
+}
