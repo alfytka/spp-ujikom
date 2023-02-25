064a8c6c50a370784239642fa0b1048c4427117c
alfytka <lyzexx04@gmail.com>
2023-02-25 13:10:45 +0700

    buktipembayaran seeder n more

--- database/seeders/PembayaranSeeder.php
+++ database/seeders/PembayaranSeeder.php
@@ -16,36 +16,40 @@ class PembayaranSeeder extends Seeder
     {
         collect([
             [
-                'petugas_id' => '1',
+                'petugas_id' => '17',
                 'siswa_id' => '1',
-                'tgl_bayar' => '2020-10-10',
-                'bulan_bayar' => 'Oktober',
-                'tahun_bayar' => '2022',
-                'jumlah_bayar' => '100000'
-            ],
-            [
-                'petugas_id' => '1',
-                'siswa_id' => '2',
-                'tgl_bayar' => '2022-9-9',
-                'bulan_bayar' => 'November',
-                'tahun_bayar' => '2022',
-                'jumlah_bayar' => '120000'
-            ],
-            [
-                'petugas_id' => '2',
-                'siswa_id' => '3',
-                'tgl_bayar' => '2022-12-12',
+                'tgl_bayar' => '2020-1-1',
                 'bulan_bayar' => 'Januari',
-                'tahun_bayar' => '2023',
-                'jumlah_bayar' => '100000'
+                'tahun_bayar' => '2020',
+                'jumlah_bayar' => '100000',
+                'jenis_pembayaran' => 'petugas'
             ],
             [
-                'petugas_id' => '3',
-                'siswa_id' => '4',
-                'tgl_bayar' => '2022-2-2',
+                'petugas_id' => '17',
+                'siswa_id' => '1',
+                'tgl_bayar' => '2020-2-2',
                 'bulan_bayar' => 'Februari',
-                'tahun_bayar' => '2023',
-                'jumlah_bayar' => '100000'
+                'tahun_bayar' => '2020',
+                'jumlah_bayar' => '100000',
+                'jenis_pembayaran' => 'petugas'
+            ],
+            [
+                'petugas_id' => '17',
+                'siswa_id' => '1',
+                'tgl_bayar' => '2020-3-3',
+                'bulan_bayar' => 'Maret',
+                'tahun_bayar' => '2020',
+                'jumlah_bayar' => '100000',
+                'jenis_pembayaran' => 'petugas'
+            ],
+            [
+                'petugas_id' => '17',
+                'siswa_id' => '2',
+                'tgl_bayar' => '2022-1-1',
+                'bulan_bayar' => 'Januari',
+                'tahun_bayar' => '2022',
+                'jumlah_bayar' => '100000',
+                'jenis_pembayaran' => 'petugas'
             ],
         ])->each(function($pembayaran) {
             Pembayaran::create($pembayaran);
