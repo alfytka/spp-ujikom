d4df66d8df83a5dc6d1fd742805a53084e53144a
alfytka <lyzexx04@gmail.com>
2023-02-25 13:23:03 +0700

    fix all of the routes web

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\FeatureController;
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\KelasController;
 use App\Http\Controllers\KompetensiKeahlianController;
+use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\ManageSiswaController;
 use App\Http\Controllers\PembayaranController;
 use App\Http\Controllers\PetugasController;
@@ -28,29 +29,50 @@ Route::get('/', [FeatureController::class, 'login'])->name('login');
 Route::post('/login', [FeatureController::class, 'loginPost']);
 Route::post('/logout', [FeatureController::class, 'logout']);
 
-Route::get('/dashboard', [FeatureController::class, 'dashboard']);
-Route::resource('/dataprodi', KompetensiKeahlianController::class);
-Route::resource('/datakelas', KelasController::class);
-Route::resource('/datasiswa', SiswaController::class);
-Route::resource('/dataspp', SppController::class);
-Route::resource('/datapetugas', PetugasController::class);
-Route::resource('/dataadmin', AdminController::class);
+Route::middleware(['admin'])->group(function() {
+    Route::get('/dashboard', [FeatureController::class, 'dashboard']);
+    Route::resource('/dataprodi', KompetensiKeahlianController::class);
+    Route::resource('/datakelas', KelasController::class);
+    Route::resource('/datasiswa', SiswaController::class);
+    Route::resource('/dataspp', SppController::class);
+    Route::resource('/datapetugas', PetugasController::class);
+    Route::resource('/dataadmin', AdminController::class);
 
-Route::resource('/datapembayaran', PembayaranController::class);
-Route::post('/datapembayaran/create', [PembayaranController::class, 'getKelas'])->name('getKelas');
-Route::post('/datapembayaran/create/siswa', [PembayaranController::class, 'getSiswa'])->name('getSiswa');
+    Route::get('/datasiswa/{id}/upload-photo', [SiswaController::class, 'uploadPhoto']);
+    Route::put('/datasiswa/{id}/upload-photo', [SiswaController::class, 'sendPhoto']);
+    Route::get('/datapetugas/{id}/upload-photo', [PetugasController::class, 'uploadPhoto']);
+    Route::put('/datapetugas/{id}/upload-photo', [PetugasController::class, 'sendPhoto']);
+    Route::get('/dataadmin/{id}/upload-photo', [AdminController::class, 'uploadPhoto']);
+    Route::put('/dataadmin/{id}/upload-photo', [AdminController::class, 'sendPhoto']);
+    
+    Route::resource('/datapembayaran', PembayaranController::class);
+    Route::get('/datapembayaran/{id}/detail-siswa', [PembayaranController::class, 'idSiswa']);
+    Route::get('/datapembayaran/{id}/proses', [PembayaranController::class, 'prosesPembayaran']);
+    Route::put('/datapembayaran/{id}/proses', [PembayaranController::class, 'updateProses']);
+    Route::get('/datapembayaran/{id}/{idsiswa}', [PembayaranController::class, 'show']);
+    
+    Route::get('/history', [HistoryController::class, 'index'])->name('history.index');
+    Route::get('/history/{id}', [HistoryController::class, 'show'])->name('history.show');
+    
+    Route::resource('/laporan', LaporanController::class);
 
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+    Route::get('/profile/edit', [ProfileController::class, 'editProfile']);
+    Route::put('/profile/{id}', [ProfileController::class, 'update']);
+    Route::get('/profile/ubahpassword', [ProfileController::class, 'ubahPassword']);
+    Route::put('/profile/ubahpassword/{id}', [ProfileController::class, 'changePassword']);
+    Route::get('/datakelas/delete/{id}', [KelasController::class, 'destroy'])->name('delete');
+});
 
-Route::resource('/history', HistoryController::class);
-Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-Route::get('/profile/edit', [ProfileController::class, 'editProfile']);
-Route::put('/profile/{id}', [ProfileController::class, 'update']);
-Route::get('/profile/ubahpassword', [ProfileController::class, 'ubahPassword']);
-Route::put('/profile/ubahpassword/{id}', [ProfileController::class, 'changePassword']);
-Route::get('/datakelas/delete/{id}', [KelasController::class, 'destroy'])->name('delete');
+Route::get('/profile-siswa', [ManageSiswaController::class, 'profile'])->name('profile-siswa.index');
+Route::get('/profile-siswa/edit', [ManageSiswaController::class, 'edit']);
+Route::put('/profile-siswa/{id}', [ManageSiswaController::class, 'update']);
+Route::get('/profile-siswa/password', [ManageSiswaController::class, 'passwordSiswa']);
+Route::put('/profile-siswa/password/{id}', [ManageSiswaController::class, 'changePassword']);
 
-Route::get('/profilesiswa', [ManageSiswaController::class, 'profile']);
-Route::get('/profilesiswa/edit', [ManageSiswaController::class, 'editProfile']);
-Route::get('/profilesiswa/ubahpassword', [ManageSiswaController::class, 'changePasswordSiswa']);
+Route::get('/siswa/{id}/beranda', [ManageSiswaController::class, 'beranda'])->name('beranda');
+Route::get('/siswa/{id}/entri-pembayaran', [ManageSiswaController::class, 'entriPembayaranSiswa']);
+Route::post('/siswa/{id}/entri-pembayaran', [ManageSiswaController::class, 'postEntriPembayaran']);
+Route::get('/siswa/{id}/riwayat-pembayaran', [ManageSiswaController::class, 'indexHistory'])->name('riwayat');
+Route::get('/siswa/{id}/fyi', [ManageSiswaController::class, 'fyi']);
 
-Route::view('/cek', 'siswa.datahistory.riwayat');
