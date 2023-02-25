36af8aaab7b5c341cf2a995b473c8067f5907121
alfytka <lyzexx04@gmail.com>
2023-02-25 12:55:59 +0700

    change the path of layouts kerangka

--- resources/views/layouts/kerangka.blade.php
+++ resources/views/layouts/kerangka.blade.php
@@ -5,46 +5,36 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>SPP - Admin</title>
+  @yield('titles')
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
-    <!-- <link href="assets/img/favicon.png" rel="icon"> -->
-    {{-- <link href="/img/apple-touch-icon.png" rel="apple-touch-icon"> --}}
-
-  <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
   <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
 
   <link rel="stylesheet" href="/vendor/extensions/simple-datatables/style.css">
   <link rel="stylesheet" href="/vendor/extensions/simple-datatables.css">
 
-  <!-- Template Main CSS File -->
   <link href="/css/style.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-  {{-- Header --}}
   <header id="header" class="header fixed-top d-flex align-items-center">
     @include('partials.admin.navbar')
   </header>
 
-  {{-- Sidebar --}}
   <aside id="sidebar" class="sidebar">
     @include('partials.admin.sidebar')
   </aside>
 
   <main id="main" class="main">
 
-    <!-- Modal -->
     <div class="modal modal-sm fade py-5" id="disablebackdrop" tabindex="-1" data-bs-backdrop="false">
       <div class="modal-dialog">
         <div class="modal-content rounded-4 shadow">
@@ -80,13 +70,13 @@
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Vendor JS Files -->
+  @yield('my-js')
+
   <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="/vendor/tinymce/tinymce.min.js"></script>
-  <script src="/vendor/extensions/simple-datatables/umd/simple-datatables.js"></script>
-  <script src="/vendor/extensions/simple-datatables.js"></script>
+  {{-- <script src="/vendor/extensions/simple-datatables/umd/simple-datatables.js"></script>
+  <script src="/vendor/extensions/simple-datatables.js"></script> --}}
 
-  <!-- Template Main JS File -->
   <script src="/js/main.js"></script>
 
 </body>
