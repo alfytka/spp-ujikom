037befaabd14979e800a5419dffdb46a2e03c286
alfytka <lyzexx04@gmail.com>
2023-03-09 16:57:11 +0700

    data kelas is fixed

--- resources/views/admin/datakelas/edit-kelas.blade.php
+++ resources/views/admin/datakelas/edit-kelas.blade.php
@@ -6,19 +6,15 @@
   
 <div class="pagetitle">
   <h5 class="fw-semibold"><a type="button" onclick="history.back()" class="back-icon"><i class="bi bi-chevron-left back-icon"></i></a> <span class="ps-1">Data Kelas</span></h5>
-</div><!-- End Page Title -->
+</div>
 
 <section class="section dashboard">
   <div class="row">
 
-    <!-- Right side columns -->
     <div class="col-lg-5">
-
-      <div class="cardxy shadow-sm">
-        <div class="card-body">
-          <h6 class="card-title"><i class="bi bi-cursor-text"></i> Ubah Data Kelas</h6>
-
-          <!-- Vertical Form -->
+      <div class="cardxy shadow-sm mb-4">
+        <div class="card-body mx-1">
+          <h6 class="card-title">Edit Data Kelas</h6>
           <form class="row g-3" action="/datakelas/{{ $datakelas->id }}" method="POST">
             @csrf
             @method('PUT')
@@ -31,69 +27,59 @@
                 @enderror
               </div>
             </div>
-              <div class="col-12">
-                <label for="dataProdi" class="form-label mb-1">Kompetensi Keahlian</label>
-                <select name="kompetensikeahlian_id" id="dataProdi" class="form-select form-select-smx roundedx @error('kompetensikeahlian_id') is-invalid @enderror">
-                  <option disabled value>- Pilih kompetensi keahlian -</option>
-                  @foreach ($dataprodi as $prodi)
-                    <option value="{{ $prodi->id }}" {{ $prodi->id == $datakelas->kompetensikeahlian_id ? 'selected' : '' }}>{{ $prodi->name }}</option>
-                  @endforeach
-                </select>
-                @error('kompetensikeahlian_id')
-                  <span class="invalid-feedback">{{ $message }}</span>
-                @enderror
-              </div>
-            <div class="text-end">
-              <button type="submit" class="btnn btn-violet py-2 px-4 mt-1 mb-3">Simpan</button>
+            <div class="col-12">
+              <label for="dataProdi" class="form-label mb-1">Kompetensi Keahlian</label>
+              <select name="kompetensikeahlian_id" id="dataProdi" class="form-select form-select-smx roundedx @error('kompetensikeahlian_id') is-invalid @enderror">
+                <option disabled value>- Pilih kompetensi keahlian -</option>
+                @foreach ($dataprodi as $prodi)
+                  <option value="{{ $prodi->id }}" {{ $prodi->id == $datakelas->kompetensikeahlian_id ? 'selected' : '' }}>{{ $prodi->name }}</option>
+                @endforeach
+              </select>
+              @error('kompetensikeahlian_id')
+                <span class="invalid-feedback">{{ $message }}</span>
+              @enderror
             </div>
-          </form><!-- Vertical Form -->
-
+            <div class="text-end">
+              <button type="submit" class="btnn btn-violet py-2 ps-4 mb-3">Simpan <i class="bi bi-chevron-right px-1"></i></button>
+            </div>
+          </form>
         </div>
       </div>
+    </div>
 
-    </div><!-- End Right side columns -->
-
-    <!-- Left side columns -->
     <div class="col-lg-7">
-        <div class="cardxy shadow-sm">
-            <div class="card-body">
-                <div class="row">
-                  <div class="col-8 col-md-8 col-lg-8">
-                    <h5 class="card-title">Preview Data Kelas</h5>
-                  </div>
-                </div>
-
-              <!-- Default Table -->
-              <div class="table-responsive">
-                <table class="table table-sm">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Nama Kelas</th>
-                      <th scope="col">Kompetensi Keahlian</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @foreach ($previewkelas as $kelas)
-                      <tr class="{{ $kelas->id == $datakelas->id ? 'table-blue' : '' }}">
-                        <th scope="row">{{ $loop->iteration }}</th>
-                        <td>{{ $kelas->kelas }}</td>
-                        <td>{{ $kelas->kompetensiKeahlian->name }}</td>
-                      </tr>
-                    @endforeach
-                  </tbody>
-                </table>
-              </div>
-              <!-- End Default Table Example -->
+      <div class="cardxy shadow-sm">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-8 col-md-8 col-lg-8">
+              <h5 class="card-title">Preview Data Kelas</h5>
             </div>
           </div>
 
-      <!-- <div class="row">
-
-      </div> -->
-    </div><!-- End Left side columns -->
+          <div class="table-responsive" style="max-height: 220px;">
+            <table class="table table-sm">
+              <thead>
+                <tr class="table-me">
+                  <th scope="col">#</th>
+                  <th scope="col"><span class="d-none d-md-inline">Nama</span> Kelas</th>
+                  <th scope="col">Kompetensi Keahlian</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($previewkelas as $kelas)
+                  <tr class="{{ $kelas->id == $datakelas->id ? 'table-blue' : '' }}">
+                    <th scope="row">{{ $loop->iteration }}</th>
+                    <td>{{ $kelas->kelas }}</td>
+                    <td>{{ $kelas->kompetensiKeahlian->name }}</td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </div>
 </section>
-
 @endsection
--- resources/views/admin/datakelas/index-kelas.blade.php
+++ resources/views/admin/datakelas/index-kelas.blade.php
@@ -5,15 +5,13 @@
 @section('content')
   
 <div class="pagetitle">
-  <h5 class="fw-semibold">Data Kelas</h5>
-</div><!-- End Page Title -->
+  <h5 class="fw-semibold">Data Kelas <span class="float-end d-inline d-md-none"><a href="#q"><i class="bi bi-plus-circle-dotted"></i> <span class="fs-12 fw-normal">Kelas</span></a></span></h5>
+</div>
 
 <section class="section dashboard">
   <div class="row">
 
-    <!-- Left side columns -->
     <div class="col-lg-8">
-
       @if (session()->has('informasi'))
         <div class="alert alert-light border-zinc roundeds alert-dismissible fade show" role="alert">
           <i class="bi bi-check-circle-fill ms-1 py-0 my-0 me-2"></i>
@@ -21,74 +19,83 @@
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
       @endif 
+      <div class="cardxy shadow-sm mb-4">
+        <div class="card-body">
 
-        <div class="cardxy shadow-sm">
-            <div class="card-body">
+          <div class="table-responsive mt-4 mt-md-3 mt-lg-3">
+            <table class="table table-sm" id="table1">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col"><span class="d-none d-md-inline">Nama</span> Kelas</th>
+                  <th scope="col">Kompetensi Keahlian</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+              @foreach ($datakelas as $kelas)
+              <tr>
+                <th scope="row">{{ $loop->iteration }}</th>
+                <td style="min-width: 95px;">{{ $kelas->kelas }}</td>
+                <td>{{ $kelas->kompetensiKeahlian->name }}</td>
+                <td class="align-middle">
+                  <div class="dropdown">
+                    <button type="button" class="btnxs btn-white" data-bs-toggle="dropdown"><i class="bi bi-three-dots-vertical"></i></button>
+                    <ul class="dropdown-menu">
+                      <li><a href="/datakelas/{{ $kelas->id }}" class="dropdown-item">Edit <i class="bi bi-pen float-end"></i></a></li>
+                      <div class="border-bottom mx-2"></div>
+                      <li><a href="" data-bs-toggle="modal" data-bs-target="#datakelas{{ $kelas->id }}" class="dropdown-item text-danger">Hapus <i class="bi bi-trash3 float-end"></i></a></li>
+                    </ul>
+                  </div>
+                </td>
 
-              <div class="table-responsive mt-4 mt-md-3 mt-lg-3">
-                <!-- Default Table -->
-                <table class="table table-sm" id="table1">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Nama Kelas</th>
-                      <th scope="col">Kompetensi Keahlian</th>
-                      <th scope="col">Aksi</th>
-                    </tr>
-                  </thead>
-                  <tbody class="">
-                  @foreach ($datakelas as $kelas)
-                    <tr>
-                      <th scope="row">{{ $loop->iteration }}</th>
-                      <td>{{ $kelas->kelas }}</td>
-                      <td>{{ $kelas->kompetensiKeahlian->name }}</td>
-                      <td>
-                        <div class="dropdown">
-                          <button type="button" class="btnxs btn-view" data-bs-toggle="dropdown"><i class="bi bi-view-list"></i></button>
-                          <ul class="dropdown-menu">
-                            <li><a href="/datakelas/{{ $kelas->id }}" class="dropdown-item">Edit <i class="bi bi-pen float-end"></i></a></li>
-                            <li>
-                              <form action="/datakelas/{{ $kelas->id }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button onclick="return confirm('Hapus data kelas?')" class="dropdown-item">Hapus <i class="bi bi-x-lg float-end"></i></button>
-                              </form>
-                            </li>
-                          </ul>
-                        </div>
-                      </td>
-                    </tr>
-                  @endforeach
-                  </tbody>
-                </table>
-                <!-- End Default Table Example -->
-              </div>
-            </div>
+                <div class="modal modal-sm fade py-5" id="datakelas{{ $kelas->id }}" tabindex="-1" data-bs-backdrop="false">
+                  <div class="modal-dialog">
+                    <div class="modal-content rounded-4 shadow">
+                      <div class="modal-header border-bottom-0">
+                        <h1 class="modal-title fs-6">Konfirmasi</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body py-0 mb-1 fs-12">
+                        Hapus data kelas?<br>
+                        <span class="text-violet fw-semibold fst-italic"> <i class="bi bi-arrow-return-right"></i> {{ $kelas->kelas }} - {{ $kelas->kompetensiKeahlian->name }}</span>
+                      </div>
+                      <div class="modal-footer flex-column border-top-0">
+                        <form action="/datakelas/{{ $kelas->id }}" method="POST">
+                          @csrf
+                          @method('DELETE')
+                          <button type="submit" class="button-9 w-100 mx-0 mb-2 roundedx">Ya, hapus</button>
+                        </form>
+                        <button type="button" class="btn-y w-100 mx-0" data-bs-dismiss="modal">Batal</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </tr>
+              @endforeach
+              </tbody>
+            </table>
           </div>
+          
+        </div>
+      </div>
+    </div>
 
-      <!-- <div class="row">
-
-      </div> -->
-    </div><!-- End Left side columns -->
-
-    <!-- Right side columns -->
-    <div class="col-lg-4">
-
-      <div class="cardxy shadow-sm">
+    <div class="col-lg-4 mb-5">
+      <div class="cardxy shadow-sm" id="q">
         <div class="card-body">
           <h5 class="card-title"><i class="bi bi-plus-lg"></i> Tambah Data Kelas</h5>
-
-          <!-- Vertical Form -->
           <form class="row g-3" action="/datakelas" method="POST">
             @csrf
-            <div class="col-12">
+            <div class="col-12 px-3 px-md-2">
               <label for="namaKelas" class="form-label mb-1">Nama Kelas</label>
               <input type="text" name="kelas" class="form-control form-control-smx roundedx @error('kelas') is-invalid @enderror" placeholder="ex: XII RPL 1" id="namaKelas" autocomplete="off" value="{{ old('kelas') }}">
               @error('kelas')
                 <span class="invalid-feedback">{{ $message }}</span>
               @enderror
             </div>
-            <div class="col-12">
+            <div class="col-12 px-3 px-md-2">
               <label for="dataProdi" class="form-label mb-1">Kompetensi Keahlian</label>
               <select name="kompetensikeahlian_id" id="dataProdi" class="form-select form-select-smx roundedx @error('kompetensikeahlian_id') is-invalid @enderror">
                 <option disabled value>- Pilih kompetensi keahlian -</option>
@@ -101,20 +108,16 @@
                 <span class="invalid-feedback">{{ $message }}</span>
               @enderror
             </div>
-            <div class="text-end">
-              <button type="submit" class="btnn btn-violet py-2 px-4 mt-1 mb-3">Simpan</button>
+            <div class="text-end px-3 px-md-2">
+              <button type="submit" class="btnn btn-violet py-2 ps-4 mb-3">Simpan <i class="bi bi-chevron-right px-1"></i></button>
             </div>
-          </form><!-- Vertical Form -->
-
+          </form>
         </div>
       </div>
-
-    </div><!-- End Right side columns -->
-
+    </div>
 
   </div>
 </section>
-
 @endsection
 
 @section('my-js')
